7df1e49f31195976e7331b304a5c1c8316426489
Anh Viet Nguyen <92666546+anhviet-key@users.noreply.github.com>
2022-08-27 19:13:00 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,6 @@ export const productConfig = () =>
   axios.get("https://data-api-json.herokuapp.com/api/products");
 export const productId = (productId) =>
   axios.get(`https://data-api-json.herokuapp.com/api/products/${productId}`);
-export const projectConfig = () => axios.get("https://data-api-json.herokuapp.com/project");
+export const projectConfig = () => axios.get("https://data-api-json.herokuapp.com/api/project");
 export const projectId = (projectId) =>
   axios.get(`https://data-api-json.herokuapp.com/api/${projectId}`);
